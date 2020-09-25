51e7edc9d4cced84cc51326e5fb2a89ee9d07d39
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-09-25 12:55:06 +0200

    sample website secured

--- src/main/java/com/ToolsApplication.java
+++ src/main/java/com/ToolsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToolsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         SpringApplication.run(ToolsApplication.class, args);
     }
-
 }
--- src/main/java/com/securingweb/MvcConfig.java
+++ src/main/java/com/securingweb/MvcConfig.java
@@ -1,4 +1,4 @@
-package com;
+package com.securingweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
