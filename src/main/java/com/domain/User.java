29812986ffac7aecdc72a1fe21d98413467330ad
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-09-27 14:29:34 +0200

    creating database & user init

--- src/main/java/com/domain/User.java
+++ src/main/java/com/domain/User.java
@@ -1,4 +1,42 @@
 package com.domain;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "birthDate")
+    private Date birthDate;
+
+    @Column(name = "pesel")
+    private String pesel;
 }
