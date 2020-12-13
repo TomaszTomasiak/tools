a0ce457a8d396869a83f43783e43d440eb7cb52d
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-13 20:07:03 +0100

    order service implemented

--- src/main/java/com/service/OrderService.java
+++ src/main/java/com/service/OrderService.java
@@ -1,4 +1,21 @@
 package com.service;
 
+import com.domain.Order;
+import com.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderService {
+    List<Order> getAllOrders();
+
+    Optional<Order> getOrder(long id);
+
+    Order saveOrder(Order booking);
+
+    Order getOrderById(long id);
+
+    void deleteOrder(long id);
+
+    List<Order> getAllOrdersOfUser(User user);
 }
--- src/main/java/com/service/OrderServiceImpl.java
+++ src/main/java/com/service/OrderServiceImpl.java
@@ -1,4 +1,43 @@
 package com.service;
 
-public class OrderServiceImpl {
+import com.domain.Order;
+import com.domain.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public List<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public Optional<Order> getOrder(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Order saveOrder(Order booking) {
+        return null;
+    }
+
+    @Override
+    public Order getOrderById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(long id) {
+
+    }
+
+    @Override
+    public List<Order> getAllOrdersOfUser(User user) {
+        return null;
+    }
 }
