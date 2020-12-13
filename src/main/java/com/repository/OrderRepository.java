f8258316feef8dc4bf3e8dc03b1552e8b87d964e
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-13 19:06:38 +0100

    implemented order repo

--- src/main/java/com/repository/OrderRepository.java
+++ src/main/java/com/repository/OrderRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    @Override
+    List<Order> findAll();
+
+    Order findOrderById(Long id);
 }
