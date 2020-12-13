32b8f0ccb8bbfb452b3a4102392f07c56e769c00
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-13 20:08:26 +0100

    order mapper implemented

--- src/main/java/com/mapper/OrderMapper.java
+++ src/main/java/com/mapper/OrderMapper.java
@@ -1,4 +1,27 @@
 package com.mapper;
 
+import com.domain.Order;
+import com.dto.OrderDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class OrderMapper {
+
+    public Order mapToOrder(final OrderDto dto) {
+        return new Order();
+    }
+
+    public OrderDto mapToDto(final Order booking) {
+        return new OrderDto(        );
+    }
+
+    public List<OrderDto> mapToDtoList(final List<Order> list) {
+        return list.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
 }
