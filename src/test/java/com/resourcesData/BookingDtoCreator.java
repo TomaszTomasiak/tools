5b607aa1d2c18060f2668560754ec56d4871f0d6
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-15 17:56:38 +0100

    userId changed to orderId due to chenges of relations in tables

--- src/test/java/com/resourcesData/BookingDtoCreator.java
+++ src/test/java/com/resourcesData/BookingDtoCreator.java
@@ -1,7 +1,5 @@
 package com.resourcesData;
 
-import com.domain.Tool;
-import com.domain.User;
 import com.dto.BookingDto;
 
 import java.time.LocalDate;
@@ -10,13 +8,13 @@ public class BookingDtoCreator {
 
     private static final long ID = 1L;
     private static final long TOOL_ID = 10L;
-    private static final long USER_ID = 28L;
+    private static final long ORDER_ID = 28L;
     private static final LocalDate BOOKED_DATE_FROM = LocalDate.of(2020, 06, 14);
     private static final LocalDate BOOKED_DATE_TO = LocalDate.of(2020, 06, 22);
 
     private static final long ID_UPDATED = 1L;
     private static final long TOOL_ID_UPDATED = 19L;
-    private static final long USER_ID_UPDATED = 30L;
+    private static final long ORDER_ID_UPDATED = 30L;
     private static final LocalDate BOOKED_DATE_FROM_UPDATED = LocalDate.of(2020, 03, 30);
     private static final LocalDate BOOKED_DATE_TO_UPDATED = LocalDate.of(2020, 04, 12);
 
@@ -24,17 +22,18 @@ public class BookingDtoCreator {
         return BookingDto.builder()
                 .id(ID)
                 .toolId(TOOL_ID)
-                .userId(USER_ID)
+                .orderId(ORDER_ID)
                 .bookedDateFrom(BOOKED_DATE_FROM)
                 .bookedDateTo(BOOKED_DATE_TO)
                 .build();
+
     }
 
     public static BookingDto updatedBookingDtoCreator() {
         return BookingDto.builder()
                 .id(ID_UPDATED)
                 .toolId(TOOL_ID_UPDATED)
-                .userId(USER_ID_UPDATED)
+                .orderId(ORDER_ID_UPDATED)
                 .bookedDateFrom(BOOKED_DATE_FROM_UPDATED)
                 .bookedDateTo(BOOKED_DATE_TO_UPDATED)
                 .build();
