e1f5063e2d2a7694209f75857515dd9777ff51f2
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-16 18:27:11 +0100

    test passed

---
+++ src/test/java/com/dao/OrderDaoTestSuite.java
@@ -0,0 +1,33 @@
+package com.dao;
+import com.domain.Order;
+import com.repository.OrderRepository;
+import com.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderDaoTestSuite {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    public void testOrderDaoSave() {
+
+        Order order = new Order();
+        orderRepository.save(order);
+        long id = order.getId();
+
+        assertEquals(0, order.getBookings().size());
+        assertNotEquals(0, id);
+    }
+}
