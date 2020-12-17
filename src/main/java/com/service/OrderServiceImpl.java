b1202f3182105c3aab8ddf22a0a3e7fd33e5eec2
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-17 20:57:13 +0100

    order service implementation

--- src/main/java/com/service/OrderServiceImpl.java
+++ src/main/java/com/service/OrderServiceImpl.java
@@ -1,43 +1,51 @@
 package com.service;
 
 import com.domain.Order;
-import com.domain.User;
+import com.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    OrderRepository repository;
+
     @Override
     public List<Order> getAllOrders() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public Optional<Order> getOrder(long id) {
-        return Optional.empty();
+        return repository.findById(id);
     }
 
     @Override
-    public Order saveOrder(Order booking) {
-        return null;
+    public Order saveOrder(Order order) {
+        return repository.save(order);
     }
 
     @Override
     public Order getOrderById(long id) {
-        return null;
+        return repository.findOrderById(id);
     }
 
     @Override
     public void deleteOrder(long id) {
-
+    repository.deleteById(id);
     }
 
     @Override
     public List<Order> getAllOrdersOfUser(long userId) {
-        return null;
+        return getAllOrders().stream()
+                .filter(o -> o.getUser().getId() == userId)
+                .collect(Collectors.toList());
     }
 }
