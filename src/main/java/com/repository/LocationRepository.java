157073c95b586e12bc91b17aa8aa3bcc5e9c7310
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-22 14:31:55 +0100

    Location repository implemented

--- src/main/java/com/repository/LocationRepository.java
+++ src/main/java/com/repository/LocationRepository.java
@@ -1,4 +1,18 @@
 package com.repository;
 
-public interface LocationRepository {
+import com.domain.Location;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Repository
+public interface LocationRepository extends CrudRepository<Location, Long> {
+
+    @Override
+    List<Location> findAll();
+
+    Location findLocationById(Long id);
 }
