5392d5ec350a8b3d44afd2acd7fbbbec33336139
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2020-12-23 16:44:45 +0100

    location service interface implemented

--- src/main/java/com/service/LocationService.java
+++ src/main/java/com/service/LocationService.java
@@ -1,4 +1,20 @@
 package com.service;
 
+import com.domain.Location;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface LocationService {
+    List<Location> getAllLocations();
+
+    Optional<Location> getLocation(long id);
+
+    Location saveLocation(Location location);
+
+    Location getLocationById(long id);
+
+    void deleteLocation(long id);
 }
