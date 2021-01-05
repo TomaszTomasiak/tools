c9abf486a29c0d0bd6ab29b3ee6c92a96689f9cd
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-05 20:40:42 +0100

    start of implemetation location mapper tests

--- src/test/java/com/mapper/LocationMapperTestSuite.java
+++ src/test/java/com/mapper/LocationMapperTestSuite.java
@@ -1,4 +1,23 @@
 package com.mapper;
 
+import com.domain.Location;
+import com.dto.LocationDto;
+import com.resourcesData.LocationDtoCreator;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LocationMapperTestSuite {
+
+    @Autowired
+    private LocationMapper mapper;
+
+    private LocationDto dto = LocationDtoCreator.locationDtoCreator();
+
+    private Location location;
+
+
 }
