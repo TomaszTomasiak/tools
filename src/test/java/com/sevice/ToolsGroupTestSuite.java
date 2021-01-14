8b75a0c4784e1acebf1dead5d59a31fdd619c299
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-14 20:42:20 +0100

    @Runwith and @SpringBootTest

--- src/test/java/com/sevice/ToolsGroupTestSuite.java
+++ src/test/java/com/sevice/ToolsGroupTestSuite.java
@@ -1,4 +1,23 @@
 package com.sevice;
 
+import com.domain.ToolsGroup;
+import com.service.ToolsGroupServiceImpl;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ToolsGroupTestSuite {
+
+    @Autowired
+    private ToolsGroupServiceImpl service;
+
+    private ToolsGroup toolsGroup = ToolsGroup.builder()
+            .id(15)
+            .name("budowlane")
+            .build();
+
+
 }
