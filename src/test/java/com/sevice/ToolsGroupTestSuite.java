866ca4900bdf653b92e9ad1889da71a4cccc1b5f
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-14 20:49:39 +0100

    testGetAllAndSave passed

--- src/test/java/com/sevice/ToolsGroupTestSuite.java
+++ src/test/java/com/sevice/ToolsGroupTestSuite.java
@@ -2,10 +2,14 @@ package com.sevice;
 
 import com.domain.ToolsGroup;
 import com.service.ToolsGroupServiceImpl;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,5 +23,17 @@ public class ToolsGroupTestSuite {
             .name("budowlane")
             .build();
 
+    @Test
+    public void testGetAllAndSave() {
+        //Given
+        long numberOfGroupsBeforeAddUser = service.getAllGroups().size();
+        //When
+        service.saveGroup(toolsGroup);
+        long numberOfGroupsAfterAddUser = service.getAllGroups().size();
 
+        //Then
+
+        assertEquals(1, numberOfGroupsAfterAddUser - numberOfGroupsBeforeAddUser);
+        assertTrue(service.getAllGroups().size() > 0);
+    }
 }
