8811e04b239d096822275b2d7a3676cad25887d7
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-02-07 12:35:52 +0100

    created and implemented CartDto

--- src/main/java/com/dto/CartDto.java
+++ src/main/java/com/dto/CartDto.java
@@ -1,4 +1,27 @@
 package com.dto;
 
+import com.domain.Booking;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CartDto {
+
+    private Long id;
+
+    private Long userId;
+
+    @Builder.Default
+    private List<Booking> bookings = new ArrayList<>();
+
+    private Long orderId;
 }
+
