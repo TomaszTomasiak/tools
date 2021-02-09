a8a1f468ee76e9a3d04943a43b1c6b82f13d59ff
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-02-09 20:13:18 +0100

    implemented Model & Producer dto's

---
+++ src/main/java/com/dto/ModelDto.java
@@ -0,0 +1,24 @@
+package com.dto;
+
+import com.domain.Tool;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ModelDto {
+    private long id;
+    private String name;
+
+    @JsonIgnore
+    private List<Tool> toolList;
+}
---
+++ src/main/java/com/dto/ProducerDto.java
@@ -0,0 +1,25 @@
+package com.dto;
+
+import com.domain.Tool;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProducerDto {
+    private long id;
+    private String name;
+
+    @JsonIgnore
+    private List<Tool> toolList;
+
+}
